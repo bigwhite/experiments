dea9a44eb7a6b3b2e5d5bc2aa115f5aaf2f3ee7e
Tony Bai <bigwhite.cn@gmail.com>
2013-10-24 22:37:59 +0800

    signal to sigaction

--- invoke_func_with_timeout_examples/timeout_wrapper.c
+++ invoke_func_with_timeout_examples/timeout_wrapper.c
@@ -6,7 +6,7 @@
 #include <signal.h>
 #include "timeout_wrapper.h"
 
-int invoke_count = 0;
+volatile int invoke_count = 0;
 sigjmp_buf invoke_env;
 
 void 
@@ -15,3 +15,26 @@ timeout_signal_handler(int sig)
     invoke_count++;
     siglongjmp(invoke_env, 1);
 }
+
+sigfunc *
+my_signal(int signo, sigfunc *func)
+{
+    struct sigaction act, oact;
+
+    act.sa_handler = func;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    if (signo == SIGALRM) {
+#ifdef SA_INTERRUPT
+        act.sa_flags |= SA_INTERRUPT;
+#endif
+    } else {
+#ifdef SA_RESTART
+        act.sa_flags |= SA_RESTART;
+#endif
+    }
+    if (sigaction(signo, &act, &oact) < 0)
+        return SIG_ERR;
+    return oact.sa_handler;
+}
+
--- invoke_func_with_timeout_examples/timeout_wrapper.h
+++ invoke_func_with_timeout_examples/timeout_wrapper.h
@@ -8,19 +8,20 @@
 #include <errno.h>
 #include <sys/time.h>
 
-extern int invoke_count;
+extern volatile int invoke_count;
 extern sigjmp_buf invoke_env;
 
 void timeout_signal_handler(int sig);
-typedef void (*sighandler_t)(int);
+typedef void sigfunc(int sig);
+sigfunc *my_signal(int signo, sigfunc* func);
 #define E_CALL_TIMEOUT (-9)
 
 /* interval: microseconds */
 #define timeout_func(func, n, interval, ret, ...) \
     { \
         invoke_count = 0; \
-        sighandler_t h = signal(SIGALRM, timeout_signal_handler); \
-        if (h == SIG_ERR) { \
+        sigfunc *sf = my_signal(SIGALRM, timeout_signal_handler); \
+        if (sf == SIG_ERR) { \
             ret = errno; \
             goto end; \
         }  \
@@ -47,7 +48,7 @@ typedef void (*sighandler_t)(int);
         ret = func(__VA_ARGS__);\
         setitimer(ITIMER_REAL, &oldtick, NULL); \
 err:\
-        signal(SIGALRM, h); \
+        my_signal(SIGALRM, sf); \
 end:\
         ;\
     }
