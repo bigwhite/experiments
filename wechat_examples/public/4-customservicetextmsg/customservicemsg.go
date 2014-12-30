54f79ec70035b8a2f8703d4b7ba4dccb69d4529f
Tony Bai <bigwhite.cn@gmail.com>
2014-12-30 15:57:52 +0800

    debug ok

--- wechat_examples/public/4-customservicetextmsg/customservicemsg.go
+++ wechat_examples/public/4-customservicetextmsg/customservicemsg.go
@@ -11,11 +11,13 @@ import (
 )
 
 const (
-	token               = "wechat4go"
-	appID               = "wx8e0fb2659c2eexxx"
-	appSecret           = "22746009b0162fe50cb915851c53fbbb"
-	accessTokenFetchUrl = "https://api.weixin.qq.com/cgi-bin/token"
+	appID                = "wx8e0fb2659c2eexxx"
+	appSecret            = "22746009b0162fe50cb915851c53fyyy"
+	accessTokenFetchUrl  = "https://api.weixin.qq.com/cgi-bin/token"
+	customServicePostUrl = "https://api.weixin.qq.com/cgi-bin/message/custom/send"
 )
+
+var openID = "oBQcwuAbKpiSAbbvd_DEZg7q27QI"
 
 type AccessTokenResponse struct {
 	AccessToken string  `json:"access_token"`
@@ -25,6 +27,24 @@ type AccessTokenResponse struct {
 type AccessTokenErrorResponse struct {
 	Errcode float64
 	Errmsg  string
+}
+
+// {
+//	"touser":"OPENID",
+//	"msgtype":"text",
+//	"text":
+//	{
+//		"content":"Hello World"
+//	}
+// }
+type CustomServiceMsg struct {
+	ToUser  string         `json:"touser"`
+	MsgType string         `json:"msgtype"`
+	Text    TextMsgContent `json:"text"`
+}
+
+type TextMsgContent struct {
+	Content string `json:"content"`
 }
 
 func fetchAccessToken() (string, float64, error) {
@@ -67,11 +87,52 @@ func fetchAccessToken() (string, float64, error) {
 	}
 }
 
+func pushCustomMsg(accessToken, toUser, msg string) error {
+	csMsg := &CustomServiceMsg{
+		ToUser:  toUser,
+		MsgType: "text",
+		Text:    TextMsgContent{Content: msg},
+	}
+
+	body, err := json.MarshalIndent(csMsg, " ", "  ")
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(body))
+
+	postReq, err := http.NewRequest("POST",
+		strings.Join([]string{customServicePostUrl, "?access_token=", accessToken}, ""),
+		bytes.NewReader(body))
+	if err != nil {
+		return err
+	}
+
+	postReq.Header.Set("Content-Type", "application/json; encoding=utf-8")
+
+	client := &http.Client{}
+	resp, err := client.Do(postReq)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+
+	return nil
+}
+
 func main() {
+	// Fetch access_token
 	accessToken, expiresIn, err := fetchAccessToken()
 	if err != nil {
 		log.Println("Get access_token error:", err)
 		return
 	}
 	fmt.Println(accessToken, expiresIn)
+
+	// Post custom service message
+	msg := "你好"
+	err = pushCustomMsg(accessToken, openID, msg)
+	if err != nil {
+		log.Println("Push custom service message err:", err)
+		return
+	}
 }
