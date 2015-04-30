76ef743ac7075952697b9ebc5798c11994bfb1f9
Tony Bai <bigwhite.cn@gmail.com>
2015-04-30 14:22:39 +0800

    add gohttps

---
+++ gohttps/1-http/server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi, This is an example of http service in golang!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
