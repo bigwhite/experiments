589bb7306f15b1dad4bf6153e97304321dc5989a
Tony Bai <bigwhite.cn@gmail.com>
2015-09-16 12:31:43 +0800

    add othertypemethodset.go

---
+++ details-in-go/3/othertypemethodset.go
@@ -0,0 +1,34 @@
+package main
+
+import "./utils"
+
+type T struct {
+}
+
+func (t T) Method1() {
+}
+
+func (t *T) Method2() {
+}
+
+func (t *T) Method3() {
+}
+
+type I interface {
+	Method1()
+	Method2()
+}
+
+func main() {
+	var t T
+	utils.DumpMethodSet(&t)
+
+	var pt = &T{}
+	utils.DumpMethodSet(&pt)
+
+	utils.DumpMethodSet((*I)(nil))
+
+	// var i I = t // cannot use t (type T) as type I in assignment: T does not implement I (Method2 method has pointer receiver)
+	var i I = pt
+	i.Method1()
+}
--- details-in-go/3/utils/utils.go
+++ details-in-go/3/utils/utils.go
@@ -5,15 +5,28 @@ import (
 	"reflect"
 )
 
+// To dump method set of type T, you should pass a pointer to T
+// to DumpMethodSet，include interface type.
+//
+// e.g.
+// for interface type I:
+//   utils.DumpMethodSet((*I)(nil))
+//
+// for non-interface type T:
+//   var t T
+//   utils.DumpMethodSet(&t)
+//
+// for non-interface type *T:
+//   var pt = &T{}
+//   utils.DumpMethodSet(&pt)
+//
 func DumpMethodSet(i interface{}) {
-	fmt.Printf("=====%T 's Method Set: =====\n", i)
-
 	v := reflect.TypeOf(i)
-	n := v.NumMethod()
-
+	elemTyp := v.Elem()
+	fmt.Printf("=====%s's method sets =======\n", elemTyp)
+	n := elemTyp.NumMethod()
 	for j := 0; j < n; j++ {
-		fmt.Println(v.Method(j).Name)
+		fmt.Println(elemTyp.Method(j).Name)
 	}
-
-	fmt.Printf("=====%T 's Method Set end =====\n\n", i)
+	fmt.Printf("=====%s's method sets end =======\n\n", elemTyp)
 }
