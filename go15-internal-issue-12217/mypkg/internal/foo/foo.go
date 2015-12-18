e1760ba924181cbf05fa2110ce3a725c0570c69f
Tony Bai <bigwhite.cn@gmail.com>
2015-12-18 09:45:44 +0800

    add source for go15-internal-issue-12217

---
+++ go15-internal-issue-12217/mypkg/internal/foo/foo.go
@@ -0,0 +1,7 @@
+package foo
+
+import "fmt"
+
+func Foo() {
+	fmt.Println("Foo in github.com/bigwhite/experiments/go15-internal-issue-12217/mypkg/internal/foo")
+}
---
+++ go15-internal-issue-12217/otherpkg/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/bigwhite/experiments/go15-internal-issue-12217/mypkg/internal/foo"
+
+func main() {
+	foo.Foo()
+}
