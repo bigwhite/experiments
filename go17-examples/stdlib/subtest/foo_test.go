34f06f3ff2a432e2263644bfb6544e8248290581
Tony Bai <bigwhite.cn@gmail.com>
2016-06-21 05:46:30 +0800

    add subtest

--- go17-examples/stdlib/subtest/foo_test.go
+++ go17-examples/stdlib/subtest/foo_test.go
@@ -9,39 +9,66 @@ var tests = []struct {
 	A, B int
 	Sum  int
 }{
-	{1, 2, 4},
-	{1, 1, 3},
-	{2, 1, 4},
+	{1, 2, 3},
+	{1, 1, 2},
+	{2, 1, 3},
 }
 
-func TestSumSubTest(t *testing.T) {
-	for _, tc := range tests {
-		//t.Run(fmt.Sprint(tc.A, "+", tc.B), func(t *testing.T) {
-		//s := fmt.Sprint(tc.A, "+", tc.B)
-		t.Run("No", func(t *testing.T) {
-			if got := tc.A + tc.B; got != tc.Sum {
-				t.Errorf("got %d; want %d", got, tc.Sum)
-			}
-		})
+func assertEqual(A, B, expect int, t *testing.T) {
+	if got := A + B; got != expect {
+		t.Errorf("got %d; want %d", got, expect)
 	}
 }
 
-func TestSumOrigin(t *testing.T) {
+/*
+func TestSumSubTest1(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(fmt.Sprint(tc.A, "+", tc.B), func(t *testing.T) {
+			assertEqual(tc.A, tc.B, tc.Sum, t)
+		})
+	}
+}
+*/
+
+func TestSumSubTest(t *testing.T) {
+	//setup code ... ...
+
+	for i, tc := range tests {
+		t.Run("A=1", func(t *testing.T) {
+			if tc.A != 1 {
+				t.Skip(i)
+			}
+			assertEqual(tc.A, tc.B, tc.Sum, t)
+		})
+
+		t.Run("A=2", func(t *testing.T) {
+			if tc.A != 2 {
+				t.Skip(i)
+			}
+			assertEqual(tc.A, tc.B, tc.Sum, t)
+		})
+	}
+
+	//teardown code ... ...
+}
+
+func TestSumInOldWay(t *testing.T) {
 	for _, tc := range tests {
 		if got := tc.A + tc.B; got != tc.Sum {
-			t.Errorf("got %d; want %d", got, tc.Sum)
+			t.Errorf("%d + %d = %d; want %d", tc.A, tc.B, got, tc.Sum)
 		}
 	}
 }
 
-func TestSumInParalell(t *testing.T) {
-	for _, tc := range tests {
-		tc := tc
-		t.Run(fmt.Sprint(tc.A, "+", tc.B), func(t *testing.T) {
-			t.Parallel()
-			if got := tc.A + tc.B; got != tc.Sum {
-				t.Errorf("got %d; want %d", got, tc.Sum)
-			}
-		})
-	}
+func TestSumSubTestInParalell(t *testing.T) {
+	t.Run("blockgroup", func(t *testing.T) {
+		for _, tc := range tests {
+			tc := tc
+			t.Run(fmt.Sprint(tc.A, "+", tc.B), func(t *testing.T) {
+				t.Parallel()
+				assertEqual(tc.A, tc.B, tc.Sum, t)
+			})
+		}
+	})
+	//teardown code
 }
