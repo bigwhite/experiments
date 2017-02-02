3c841c0cee29d67d9255acdcb6f0ed5f8b215b80
Tony Bai <bigwhite.cn@gmail.com>
2017-02-02 22:24:50 +0800

    add some init func for plugin

--- go18-examples/gotoolchain/plugins/foo.go
+++ go18-examples/gotoolchain/plugins/foo.go
@@ -1,5 +1,11 @@
 package main
 
+import "fmt"
+
+func init() {
+	fmt.Println("init function in plugin foo")
+}
+
 func Foo(in string) string {
 	return "hello, " + in
 }
--- go18-examples/gotoolchain/plugins/main.go
+++ go18-examples/gotoolchain/plugins/main.go
@@ -5,8 +5,14 @@ import (
 	"plugin"
 )
 
+func init() {
+	fmt.Println("init in main program")
+}
+
 func main() {
+	fmt.Println("before opening the foo.so")
 	p, _ := plugin.Open("foo.so")
+	fmt.Println("after opening the foo.so")
 	f, _ := p.Lookup("Foo")
 	fmt.Println(f.(func(string) string)("gophers"))
 }
