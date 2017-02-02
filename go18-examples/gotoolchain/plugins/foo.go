9130e055e560fafd333604370d13c40b815e7702
Tony Bai <bigwhite.cn@gmail.com>
2017-02-02 22:40:27 +0800

    add some unexported symbols

--- go18-examples/gotoolchain/plugins/foo.go
+++ go18-examples/gotoolchain/plugins/foo.go
@@ -2,10 +2,19 @@ package main
 
 import "fmt"
 
+var V int
+var v int
+
 func init() {
+	V = 17
+	v = 23
 	fmt.Println("init function in plugin foo")
 }
 
 func Foo(in string) string {
+	return "Hello, " + in
+}
+
+func foo(in string) string {
 	return "hello, " + in
 }
--- go18-examples/gotoolchain/plugins/main.go
+++ go18-examples/gotoolchain/plugins/main.go
@@ -10,9 +10,41 @@ func init() {
 }
 
 func main() {
+	var err error
 	fmt.Println("before opening the foo.so")
-	p, _ := plugin.Open("foo.so")
+
+	p, err := plugin.Open("foo.so")
+	if err != nil {
+		fmt.Println("plugin Open error:", err)
+		return
+	}
 	fmt.Println("after opening the foo.so")
-	f, _ := p.Lookup("Foo")
-	fmt.Println(f.(func(string) string)("gophers"))
+
+	f, err := p.Lookup("Foo")
+	if err != nil {
+		fmt.Println("plugin Lookup symbol Foo error:", err)
+	} else {
+		fmt.Println(f.(func(string) string)("gophers"))
+	}
+
+	f, err = p.Lookup("foo")
+	if err != nil {
+		fmt.Println("plugin Lookup symbol foo error:", err)
+	} else {
+		fmt.Println(f.(func(string) string)("gophers"))
+	}
+
+	v, err := p.Lookup("V")
+	if err != nil {
+		fmt.Println("plugin Lookup symbol V error:", err)
+	} else {
+		fmt.Println(*v.(*int))
+	}
+
+	v, err = p.Lookup("v")
+	if err != nil {
+		fmt.Println("plugin Lookup symbol v error:", err)
+	} else {
+		fmt.Println(*v.(*int))
+	}
 }
