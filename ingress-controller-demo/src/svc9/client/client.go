d33c86ae0e15cd87ddb985c93b6f5aa91d0a9e92
Tony Bai <baim@neusoft.com>
2018-06-24 23:20:20 +0800

    ingress demo: make svc9 check client crt

---
+++ ingress-controller-demo/src/svc9/client/client.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	pool := x509.NewCertPool()
+	caCertPath := "rootCA.pem"
+
+	caCrt, err := ioutil.ReadFile(caCertPath)
+	if err != nil {
+		fmt.Println("ReadFile err:", err)
+		return
+	}
+	pool.AppendCertsFromPEM(caCrt)
+
+	cliCrt, err := tls.LoadX509KeyPair("client.crt", "client.key")
+	if err != nil {
+		fmt.Println("loadx509keypair err:", err)
+		return
+	}
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			RootCAs:      pool,
+			Certificates: []tls.Certificate{cliCrt},
+		},
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Get("https://localhost:8080")
+
+	if err != nil {
+		// handle error
+		fmt.Println("error:", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+}
--- ingress-controller-demo/src/svc9/main.go
+++ ingress-controller-demo/src/svc9/main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+type myhandler struct {
+}
+
+func (h *myhandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Println("receive a request")
 	fmt.Fprintf(w, "Hello, I am svc9 for ingress-controller demo!\n")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServeTLS(":8080", "cert.pem", "key.pem", nil)
+	pool := x509.NewCertPool()
+	caCertPath := "rootCA.pem"
+
+	caCrt, err := ioutil.ReadFile(caCertPath)
+	if err != nil {
+		fmt.Println("ReadFile err:", err)
+		return
+	}
+
+	pool.AppendCertsFromPEM(caCrt)
+
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: &myhandler{},
+		TLSConfig: &tls.Config{
+			ClientCAs:  pool,
+			ClientAuth: tls.RequireAndVerifyClientCert,
+		},
+	}
+
+	err = s.ListenAndServeTLS("server.crt", "server.key")
+	if err != nil {
+		fmt.Println("ListenAndServeTLS err:", err)
+	}
+	fmt.Println("ListenAndServeTLS exit ok")
 }
