0b251fd91171710958acf3ba7f32957674226f94
Tony Bai <bigwhite.cn@aliyun.com>
2021-07-25 21:27:28 +0800

    rename some parameter

--- tcp-stream-proto/demo1/cmd/client/main.go
+++ tcp-stream-proto/demo1/cmd/client/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"time"
+	"sync"
 
 	"github.com/bigwhite/tcp-stream-proto/demo1/pkg/frame"
 	"github.com/bigwhite/tcp-stream-proto/demo1/pkg/packet"
@@ -12,6 +12,21 @@ import (
 )
 
 func main() {
+	var wg sync.WaitGroup
+	var num int = 5
+
+	wg.Add(5)
+
+	for i := 0; i < num; i++ {
+		go func() {
+			defer wg.Done()
+			startClient()
+		}()
+	}
+	wg.Wait()
+}
+
+func startClient() {
 	conn, err := net.Dial("tcp", ":8888")
 	if err != nil {
 		log.Println("dial error:", err)
@@ -28,6 +43,25 @@ func main() {
 	frameCodec := frame.NewMyFrameCodec()
 	var counter int
 
+	go func() {
+		for {
+			// handle ack
+			// read from the connection
+			ackFramePayLoad, err := frameCodec.Decode(conn)
+			if err != nil {
+				panic(err)
+			}
+
+			p, err := packet.Decode(ackFramePayLoad)
+
+			submitAck, ok := p.(*packet.SubmitAck)
+			if !ok {
+				panic("not submitack")
+			}
+			fmt.Printf("the result of submit ack[%s] is %d\n", submitAck.ID, submitAck.Result)
+		}
+	}()
+
 	for {
 		// send submit
 		counter++
@@ -37,33 +71,19 @@ func main() {
 			ID:      id,
 			Payload: []byte(payload),
 		}
-		fmt.Printf("send submit id = %s, payload=%s\n", s.ID, s.Payload)
 
 		framePayload, err := packet.Encode(s)
 		if err != nil {
 			panic(err)
 		}
 
+		fmt.Printf("send submit id = %s, payload=%s, frame length = %d\n", s.ID, s.Payload, len(framePayload)+4)
+
 		err = frameCodec.Encode(conn, framePayload)
 		if err != nil {
 			panic(err)
 		}
 
-		// handle ack
-		// read from the connection
-		ackFramePayLoad, err := frameCodec.Decode(conn)
-		if err != nil {
-			panic(err)
-		}
-
-		p, err := packet.Decode(ackFramePayLoad)
-
-		submitAck, ok := p.(*packet.SubmitAck)
-		if !ok {
-			panic("not submitack")
-		}
-		fmt.Printf("the result of submit ack[%s] is %d\n", submitAck.ID, submitAck.Result)
-
-		time.Sleep(5 * time.Second)
+		//	time.Sleep(1 * time.Second)
 	}
 }
