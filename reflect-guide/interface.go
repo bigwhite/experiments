2b926aba74f4af876774b2ed8a8e097cc4ad23e8
Tony Bai <bigwhite.cn@aliyun.com>
2023-07-08 23:40:44 +0800

    update interface.go

--- reflect-guide/interface.go
+++ reflect-guide/interface.go
@@ -18,10 +18,10 @@ func (c Cat) Speak() string {
 func main() {
 	var a Animal = Cat{}
 	typ := reflect.TypeOf(a)
-	fmt.Println(typ.Kind())         // interface
+	fmt.Println(typ.Kind())         // struct
 	fmt.Println(typ.NumMethod())    // 1
 	fmt.Println(typ.Method(0).Name) // Speak
-	fmt.Println(typ.Method(0).Type) // func(main.Animal) string
+	fmt.Println(typ.Method(0).Type) // func(main.Cat) string
 
 	fmt.Println(reflect.ValueOf(a).Type()) // main.Cat
 }
